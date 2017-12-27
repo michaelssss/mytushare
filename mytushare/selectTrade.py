b65da0c3a8f56bb33027c17d0690a8e7ed4d06c8
梁煜铭 <yuming_liang@daydao.com>
2017-12-27 14:24:10 +0800

    no

--- mytushare/selectTrade.py
+++ mytushare/selectTrade.py
@@ -4,23 +4,29 @@ import sys
 import tushare as ts
 
 
-# 以二十天均价为准点，偏离均价两个点即可入选
-def select(duration, offsetrate):
+# 如果近duration的均值偏离值小于offsetrate且近duration2均值偏离值大于offsetrate2则入选
+def select(duration, offsetrate, duration2, offsetrate2):
     stocks = ts.get_stock_basics()
     now = dt.datetime.now()
     before = (dt.datetime.now() - dt.timedelta(days=duration))
+    before2 = (dt.datetime.now() - dt.timedelta(days=duration2))
     for stockCode, row in stocks.iterrows():
         # print("analyze name=", row['name'], " code=", stockCode)
         df = ts.get_k_data(code=stockCode, start=before.strftime("%Y-%m-%d"),
                            end=now.strftime("%Y-%m-%d"))
         avgPrice = (get_average_open_price_in_duration(df) + get_average_close_price_in_duration(df)) / 2
+        df2 = ts.get_k_data(code=stockCode, start=before2.strftime("%Y-%m-%d"),
+                            end=now.strftime("%Y-%m-%d"))
+        avgPrice2 = (get_average_open_price_in_duration(df2) + get_average_close_price_in_duration(df2)) / 2
+
         lastday = (dt.datetime.now() - dt.timedelta(days=1))
         df1 = ts.get_k_data(code=stockCode, start=lastday.strftime("%Y-%m-%d"),
                             end=lastday.strftime("%Y-%m-%d"))
         for _, row1 in df1.iterrows():
             lastdayprice = row1['close']
             offrate = ((lastdayprice - avgPrice) / avgPrice) * 100
-            if offrate < offsetrate:
+            offrate2 = ((lastdayprice - avgPrice2) / avgPrice2) * 100
+            if offrate < offsetrate and offrate2 > offsetrate2:
                 print('buy name=', row['name'], " code=", stockCode, " offsetrate=%.2f%%" % offrate)
 
 
