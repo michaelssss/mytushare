c493cab7f24e51a977c4477716652e87478d5464
梁煜铭 <yuming_liang@daydao.com>
2017-12-27 13:31:59 +0800

    no

--- mytushare/selectTrade.py
+++ mytushare/selectTrade.py
@@ -2,8 +2,6 @@ import datetime as dt
 
 import tushare as ts
 
-import mytushare.tradeInfo as my
-
 
 # 以二十天均价为准点，偏离均价两个点即可入选
 def select(duration):
@@ -14,7 +12,7 @@ def select(duration):
         print("analyze ", row['code'])
         df = ts.get_k_data(code=row['code'], start=before.strftime("%Y-%m-%d"),
                            end=now.strftime("%Y-%m-%d"))
-        avgPrice = (my.get_average_open_price_in_duration(df) + my.get_average_close_price_in_duration(df)) / 2
+        avgPrice = (get_average_open_price_in_duration(df) + get_average_close_price_in_duration(df)) / 2
         lastday = (dt.datetime.now() - dt.timedelta(days=1))
         df1 = ts.get_k_data(code=row['code'], start=lastday.strftime("%Y-%m-%d"),
                             end=lastday.strftime("%Y-%m-%d"))
@@ -23,6 +21,24 @@ def select(duration):
             offrate = ((lastdayprice - avgPrice) / avgPrice) * 100
             if offrate > 2:
                 print(row['code'])
+
+
+def get_average_close_price_in_duration(df):
+    index = 0
+    total = 0
+    for _, row in df.iterrows():
+        total += row['close']
+        index += 1
+    return total / index
+
+
+def get_average_open_price_in_duration(df):
+    index = 0
+    total = 0
+    for _, row in df.iterrows():
+        total += row['open']
+        index += 1
+    return total / index
 
 
 select(5)
