814cea958c02c6c5ac077464e62233bab5015ef6
梁煜铭 <yuming_liang@daydao.com>
2017-12-28 13:58:37 +0800

    多线程计算

--- mytushare/selectTrade.py
+++ mytushare/selectTrade.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import threading as thread
 
 import tushare as ts
 
@@ -12,20 +13,24 @@ def select(duration, offsetrate, duration2, offsetrate2):
         now = dt.datetime.now()
         file.writelines(now.strftime("%Y-%m-%d %H:%M:%S \r\n"))
         for stockCode, row in stocks.iterrows():
-            avgPrice = utils.CalcAvg(duration, stockCode)
-            avgPrice2 = utils.CalcAvg(duration2, stockCode)
-            lastday = (dt.datetime.now() - dt.timedelta(days=1))
-            df1 = ts.get_k_data(code=stockCode, start=lastday.strftime("%Y-%m-%d"),
-                                end=lastday.strftime("%Y-%m-%d"))
-            for _, row1 in df1.iterrows():
-                lastdayprice = row1['close']
-                offrate = ((lastdayprice - avgPrice) / avgPrice) * 100
-                offrate2 = ((lastdayprice - avgPrice2) / avgPrice2) * 100
-                if offrate < offsetrate and offrate2 > offsetrate2:
-                    file.write(
-                        str.format("name={0}, code={1}, offsetrate={2:.2f}%  ,offsetrate2={3:.2f}% \r\n", row['name'],
-                                   stockCode, offrate, offrate2))
-                    file.flush()
+            thread.Thread(target=cal, args=[duration, duration2, file, offsetrate, offsetrate2, row, stockCode]).start()
+
+
+def cal(duration, duration2, file, offsetrate, offsetrate2, row, stockCode):
+    avgPrice = utils.CalcAvg(duration, stockCode)
+    avgPrice2 = utils.CalcAvg(duration2, stockCode)
+    lastday = (dt.datetime.now() - dt.timedelta(days=1))
+    df1 = ts.get_k_data(code=stockCode, start=lastday.strftime("%Y-%m-%d"),
+                        end=lastday.strftime("%Y-%m-%d"))
+    for _, row1 in df1.iterrows():
+        lastdayprice = row1['close']
+        offrate = ((lastdayprice - avgPrice) / avgPrice) * 100
+        offrate2 = ((lastdayprice - avgPrice2) / avgPrice2) * 100
+        if offrate < offsetrate and offrate2 > offsetrate2:
+            file.write(
+                str.format("name={0}, code={1}, offsetrate={2:.2f}%  ,offsetrate2={3:.2f}% \r\n", row['name'],
+                           stockCode, offrate, offrate2))
+            file.flush()
 
 
 def main(duration, offsetrate, duration2, offsetrate2):
