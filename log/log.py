d7c9b9fc3969e548f86a20a7edde679e7328a4d6
michaelssss <liangyuming2@gmail.com>
2018-03-08 08:44:12 +0800

    增加日志记录的功能

--- log/log.py
+++ log/log.py
@@ -8,9 +8,9 @@ DEBUG = "debug"
 class log:
     __instance = None
 
-    def __new__(cls):
+    def __new__(cls, *str):
         if not cls.__instance:
-            log.__instance = object.__new__(cls)
+            log.__instance = object.__new__(cls, str)
         return cls.__instance
 
     def __init__(self, *str):
